662be2946a76a71e88ebca36b8620fb67dcc6adb
stackgik <stackgik@gmail.com>
2023-08-13 22:57:57 +0100

    slides added to the hero section

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use strict";
+const swiper = new Swiper(".header", {
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  autoplayDelay: "2500",
+  autoplayDisableOnInteraction: "false",
+});
+
+console.log(swiper);
