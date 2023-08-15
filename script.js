c949667cd311d47bb61dc3aaadc427fa8db7dcb2
stackgik <stackgik@gmail.com>
2023-08-15 04:07:22 +0100

    Swiper js library working just fine now

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 "use strict";
-const swiper = new Swiper(".header", {
+//code for the Swiper.js
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+const swiper = new Swiper(".hero__section", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-
-  autoplayDelay: "2500",
-  autoplayDisableOnInteraction: "false",
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    },
+  },
+  navigation: true,
+  grabCursor: true,
 });
-
-console.log(swiper);
