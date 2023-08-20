77c43ce5d4ba96f6919b5d2147f3154526d4babc
stackgik <stackgik@gmail.com>
2023-08-20 22:20:13 +0100

    tabbed component was implemented

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 //code for the Swiper.js
 const progressCircle = document.querySelector(".autoplay-progress svg");
 const progressContent = document.querySelector(".autoplay-progress span");
-const swiper = new Swiper(".hero__section", {
-  spaceBetween: 30,
+const heroSection = new Swiper(".hero__section", {
+  spaceBetween: 0,
   centeredSlides: true,
   autoplay: {
     delay: 2500,
@@ -22,4 +22,43 @@ const swiper = new Swiper(".hero__section", {
   },
   navigation: true,
   grabCursor: true,
+});
+
+const birthdayCakes = new Swiper(".swiper1", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  navigation: true,
+  grabCursor: true,
+});
+
+// ? Script for the treats menu functionality
+const treatsMenu = document.querySelector(".treats__menu"),
+  treatsItems = document.querySelectorAll(".treats__item"),
+  sectionContainers = document.querySelectorAll(".section__container"),
+  underline = document.querySelector(".line");
+
+treatsMenu.addEventListener("click", (e) => {
+  const clicked = e.target.closest(".treats__item");
+  console.log(clicked);
+
+  // Guard Clause
+  if (!clicked) return;
+
+  // adding the underline functionalities
+  underline.style.width = clicked.offsetWidth + "px";
+  underline.style.left = clicked.offsetLeft + "px";
+
+  // resetting all the classes
+  treatsItems.forEach((item) => item.classList.remove("treats__item--active"));
+
+  sectionContainers.forEach((container) =>
+    container.classList.remove("section__container--active")
+  );
+
+  // adding the classes
+  document
+    .querySelector(`.section__container--${clicked.dataset.item}`)
+    .classList.add("section__container--active");
+
+  clicked.classList.add("treats__item--active");
 });
