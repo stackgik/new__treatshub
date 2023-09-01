ad06cb0f18f4d57a5c753f0ae2ab17a4e1b11f5f
stackgik <stackgik@gmail.com>
2023-09-01 22:33:05 +0100

    more changes have been added to the JS file

--- script.js
+++ script.js
@@ -95,30 +95,52 @@ const menu = document.querySelector(".nav__lists"),
   submenus = document.querySelectorAll(".submenu"),
   linkIcons = document.querySelectorAll(".nav__link i");
 
+const resetLinks = () => {
+  // doing general resetting
+  navLinks.forEach((navLink) => navLink.classList.remove("link--active"));
+  submenus.forEach((submenu) => submenu.classList.remove("submenu--active"));
+  linkIcons.forEach((icon) => icon.classList.remove("link--active"));
+  linkIcons.forEach((icon) => (icon.style.transform = "rotate(0)"));
+};
+
+// ! This code runs each time a click is made on the links with nav__item class
 menu.addEventListener("click", (event) => {
   const clicked = event.target.closest(".nav__item");
 
   // using the guard clause
   if (!clicked) return;
 
-  // doing general resetting
-  navLinks.forEach((navLink) => navLink.classList.remove("link--active"));
-  submenus.forEach((submenu) => submenu.classList.remove("submenu--active"));
-  linkIcons.forEach((icon) => icon.classList.remove("link--active"));
-  linkIcons.forEach((icon) => (icon.style.transform = "rotate(0)"));
-
-  // selecting and adding the necessary classes
-  const clickedLink = document.querySelector(
+  // selecting the elements involved
+  const clickedLink = clicked.querySelector(
     `.nav__link--${clicked.dataset.page}`
   );
-  clickedLink.classList.toggle("link--active");
 
-  const showSubmenu = document.querySelector(
+  const showSubmenu = clicked.querySelector(
     `.submenu--${clicked.dataset.page}`
   );
-  showSubmenu.classList.toggle("submenu--active");
 
-  const rotateIcon = document.querySelector(`.icon--${clicked.dataset.page}`);
-  rotateIcon.classList.toggle("link--active");
-  rotateIcon.style.transform = "rotate(180deg)";
+  const rotateIcon = clicked.querySelector(`.icon--${clicked.dataset.page}`);
+
+  // this selects the main link inside the e.target
+  const isLinkActive = clickedLink.classList.contains("link--active");
+
+  // It checks if the link is already open, then does a general reset
+  if (isLinkActive) resetLinks();
+
+  // If not open, add these classes
+  if (!isLinkActive) {
+    // adding the necessary classes if the link isn't already active
+    clickedLink.classList.add("link--active");
+    showSubmenu.classList.add("submenu--active");
+    rotateIcon.classList.add("link--active");
+    rotateIcon.style.transform = "rotate(180deg)";
+  }
+});
+
+// a click even on the body to also close the submenu
+const body = document.querySelector("body");
+body.addEventListener("click", (event) => {
+  if (!event.target.closest(".nav__item")) {
+    resetLinks();
+  }
 });
