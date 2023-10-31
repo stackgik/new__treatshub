f51fc4dd98db0209708435903ea21c8bb12febf9
-stackgik <stackgik@gmail.com>
2023-10-31 11:59:29 +0100

    add swiper CDN to the page2 html file to load the page, and also refactor the pagination code into a class.

--- src/JS_files/pages.js
+++ src/JS_files/pages.js
@@ -1,71 +1,93 @@
 import { toggleSubmenu, revealSections } from './main.js';
 
+const previousBtn = document.querySelector('.previous__btn'),
+    nextBtn = document.querySelector('.next__btn'),
+    pageLinks = document.querySelectorAll('.pagination__link');
+
 toggleSubmenu();
 revealSections();
 
 // *************************************
 
-// refactor to class
 class Pagination {
-    constructor() {}
+    currentPage;
+
+    constructor() {
+        this.renderPage();
+    }
+
+    renderPage() {
+        if (Number.parseInt(this.getStorageItem()) === 1) {
+            previousBtn.ariaDisabled = true;
+            previousBtn.classList.add('page__link--disabled');
+        }
+
+        this.currentPage = this.getStorageItem();
+
+        if (this.currentPage === null) {
+            this.currentPage = 1; //which means nothing has been set, hence the default value.
+            this.setStorageItem();
+        }
+
+        pageLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                this.currentPage = link.dataset.page;
+                link.href = `/src/products/birthday-cakes/page_${this.currentPage}.html`;
+                this.updateLink();
+                this.setStorageItem();
+            });
+        });
+
+        this.updateLink();
+
+        previousBtn.addEventListener('click', this.loadPreviousPage.bind(this));
+
+        this.updateLink();
+
+        nextBtn.addEventListener('click', this.loadnextPage.bind(this));
+        this.updateLink();
+    }
+
+    updateLink() {
+        pageLinks.forEach(link =>
+            link.classList.remove('pagination__link--active')
+        );
+        pageLinks[this.currentPage - 1].classList.add(
+            'pagination__link--active'
+        );
+    }
+
+    loadPreviousPage() {
+        previousBtn.href = `/src/products/birthday-cakes/page_${
+            Number.parseInt(this.getStorageItem()) - 1
+        }.html`;
+
+        this.updateLink();
+
+        this.currentPage = Number.parseInt(this.getStorageItem()) - 1;
+
+        this.setStorageItem();
+    }
+
+    loadnextPage() {
+        nextBtn.href = `/src/products/birthday-cakes/page_${
+            Number.parseInt(this.getStorageItem()) + 1
+        }.html`;
+
+        this.updateLink();
+
+        this.currentPage = Number.parseInt(this.getStorageItem()) + 1;
+
+        this.setStorageItem();
+    }
+
+    setStorageItem() {
+        return sessionStorage.setItem('currentPage', this.currentPage);
+    }
+
+    getStorageItem() {
+        return sessionStorage.getItem('currentPage');
+    }
 }
 
-// functionality for the pagination
-const previousBtn = document.querySelector('.previous__btn'),
-    nextBtn = document.querySelector('.next__btn'),
-    links = document.querySelectorAll('.pagination__link');
-
-// A function to update the active page
-const updateLink = () => {
-    links.forEach(link => link.classList.remove('pagination__link--active'));
-    links[currentPage - 1].classList.add('pagination__link--active');
-};
-
-//A function for the previous button
-const loadPreviousPage = () => {
-    previousBtn.href = `/src/products/birthday-cakes/page_${
-        Number.parseInt(sessionStorage.getItem('currentPage')) - 1
-    }.html`;
-    updateLink();
-    currentPage = Number.parseInt(sessionStorage.getItem('currentPage')) - 1;
-    sessionStorage.setItem('currentPage', currentPage);
-};
-
-// A function for the next button
-const loadnextPage = () => {
-    nextBtn.href = `/src/products/birthday-cakes/page_${
-        Number.parseInt(sessionStorage.getItem('currentPage')) + 1
-    }.html`;
-    updateLink();
-    currentPage = Number.parseInt(sessionStorage.getItem('currentPage')) + 1;
-    sessionStorage.setItem('currentPage', currentPage);
-};
-
-if (Number.parseInt(sessionStorage.getItem('currentPage')) === 1) {
-    previousBtn.ariaDisabled = true;
-    previousBtn.classList.add('page__link--disabled');
-}
-
-let currentPage = sessionStorage.getItem('currentPage');
-
-if (currentPage === null) {
-    currentPage = 1; //which means nothing has been set, hence the default value.
-    sessionStorage.setItem('currentPage', currentPage);
-}
-
-links.forEach(link => {
-    link.addEventListener('click', () => {
-        currentPage = link.dataset.page;
-        link.href = `/src/products/birthday-cakes/page_${currentPage}.html`;
-        updateLink();
-        sessionStorage.setItem('currentPage', currentPage);
-    });
-});
-
-updateLink();
-
-previousBtn.addEventListener('click', loadPreviousPage);
-updateLink();
-
-nextBtn.addEventListener('click', loadnextPage);
-updateLink();
+const pagination = new Pagination();
